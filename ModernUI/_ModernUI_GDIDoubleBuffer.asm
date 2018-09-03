d25ecca5db0958ca6f7729791c2caa86650d7b39
mrfearless <fearless.is.my.name@gmail.com>
2018-09-03 18:11:43 +0100

    Update MUIGDIDoubleBuffer function

--- ModernUI/_ModernUI_GDIDoubleBuffer.asm
+++ ModernUI/_ModernUI_GDIDoubleBuffer.asm
@@ -37,29 +37,28 @@ MUI_ALIGN
 ; MUIGDIDoubleBufferStart - Starts double buffering. Used in a WM_PAINT event. 
 ; Place after BeginPaint call
 ;------------------------------------------------------------------------------
-MUIGDIDoubleBufferStart PROC USES EBX hWin:DWORD, hdcSource:HDC, lpHDCBuffer:DWORD, lpClientRect:DWORD, lpBufferBitmap:DWORD, lpPreBufferBitamp:DWORD
+MUIGDIDoubleBufferStart PROC USES EBX hWin:DWORD, hdcSource:HDC, lpHDCBuffer:DWORD, lpClientRect:DWORD, lphBufferBitmap:DWORD
     LOCAL hdcBuffer:DWORD
-    LOCAL hBitmap:DWORD
+    LOCAL hBufferBitmap:DWORD
 
-    .IF lpHDCBuffer == 0 || lpClientRect == 0 || lpBufferBitmap == 0 || lpPreBufferBitamp == 0
-        mov eax, FALSE
-        ret
+    .IF lpHDCBuffer == 0 || lpClientRect == 0 || lphBufferBitmap == 0
+        mov eax, FALSE                              ; Need all these to not be 0
+        ret                                         ; so we need to fail if none are set
     .ENDIF
-    Invoke GetClientRect, hWin, lpClientRect
-    Invoke CreateCompatibleDC, hdcSource
-    mov hdcBuffer, eax
-    mov ebx, lpHDCBuffer
-    mov [ebx], eax
-    mov ebx, lpClientRect
+    Invoke GetClientRect, hWin, lpClientRect        ; Get dimensions of area to buffer
+    Invoke CreateCompatibleDC, hdcSource            ; Create memory dc for our buffer
+    mov hdcBuffer, eax                              ; Store memory dc created in a local variable
+    mov ebx, lpHDCBuffer                            ; Get lpHDCBuffer
+    mov [ebx], eax                                  ; Place hdcBuffer (in eax) into var pointed to by lpHDCBuffer (in ebx)
+    Invoke SaveDC, hdcBuffer                        ; Save hdcBuffer status for later restore
+    mov ebx, lpClientRect                           ; Create bitmap of size that matches dimensions
     Invoke CreateCompatibleBitmap, hdcSource, [ebx].RECT.right, [ebx].RECT.bottom
-    mov hBitmap, eax
-    mov ebx, lpBufferBitmap
-    mov [ebx], eax
-    Invoke SelectObject, hdcBuffer, hBitmap
-    mov ebx, lpPreBufferBitamp
-    mov [ebx], eax
-    mov eax, TRUE
-    ret
+    mov hBufferBitmap, eax                          ; Store bitmap created in a local variable
+    mov ebx, lphBufferBitmap                        ; Get lphBufferBitmap
+    mov [ebx], eax                                  ; Place hBufferBitmap (in eax) into var pointed to by lphBufferBitmap (in ebx)
+    Invoke SelectObject, hdcBuffer, hBufferBitmap   ; Select our created buffer bitmap into our memory dc
+    mov eax, TRUE                                   ; When we later use hdcBuffer for drawing, we are actually using  
+    ret                                             ; hBufferBitmap, which we will BitBlt back to hdcSource later on
 MUIGDIDoubleBufferStart ENDP
 
 
@@ -67,19 +66,34 @@ MUI_ALIGN
 ;------------------------------------------------------------------------------
 ; MUIGDIDoubleBufferFinish - Finishes double buffering - cleans up afterwards.
 ; Used in a WM_PAINT event. Place before EndPaint call and after all Blt calls
+;
+; hBitmapUsed, hFontUsed, hBrushUsed, and hPenUsed are optional parameters.
+; If you have used a bitmap image (not the double buffer bitmap which
+; is hBufferBitmap) or a font, brush or pen in your code in the hdcBuffer 
+; you can pass the handles here for cleaning up, otherwise pass 0 for the
+; parameters that you havent used.
 ;------------------------------------------------------------------------------
-MUIGDIDoubleBufferFinish PROC hdcBuffer:HDC, hBufferBitmap:HBITMAP, hPreBufferBitamp:HBITMAP
-    .IF hBufferBitmap != 0
-        Invoke SelectObject, hdcBuffer, hBufferBitmap
-        Invoke DeleteObject, hBufferBitmap
-    .ENDIF
-    .IF hPreBufferBitamp != 0
-        Invoke SelectObject, hdcBuffer, hPreBufferBitamp
-        Invoke DeleteObject, hPreBufferBitamp
-    .ENDIF
+MUIGDIDoubleBufferFinish PROC hdcBuffer:HDC, hBufferBitmap:HBITMAP, hBitmapUsed:HBITMAP, hFontUsed:HFONT, hBrushUsed:HBRUSH, hPenUsed:HPEN
     .IF hdcBuffer != 0
-        Invoke DeleteDC, hdcBuffer
+        Invoke RestoreDC, hdcBuffer, -1         ; restore last saved state, which is just after hdcBuffer was created
+        .IF hBitmapUsed != 0
+            Invoke DeleteObject, hBitmapUsed    ; Delete optional bitmap image - if a bitmap image was used at all 
+        .ENDIF
+        .If hFontUsed != 0
+            Invoke DeleteObject, hFontUsed      ; Delete optional font - if a font was used at all
+        .ENDIF
+        .If hBrushUsed != 0
+            Invoke DeleteObject, hBrushUsed     ; Delete optional brush - if a brush was used at all
+        .ENDIF
+        .If hPenUsed != 0
+            Invoke DeleteObject, hPenUsed       ; Delete optional pen - if a pen was used at all
+        .ENDIF        
+        .IF hBufferBitmap != 0
+            Invoke DeleteObject, hBufferBitmap  ; Delete bitmap used for double buffering
+        .ENDIF
+        Invoke DeleteDC, hdcBuffer              ; Delete double buffer hdc
     .ENDIF
+    xor eax, eax
     ret
 MUIGDIDoubleBufferFinish ENDP
 
